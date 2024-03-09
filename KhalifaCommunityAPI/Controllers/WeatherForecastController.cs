d04083772303f038a78d32741ac31c5ed647923c
Fezalkhalifa <63090868+Fezalkhalifa@users.noreply.github.com>
2024-03-09 11:32:17 -0500

    added summary

--- KhalifaCommunityAPI/Controllers/WeatherForecastController.cs
+++ KhalifaCommunityAPI/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace KhalifaCommunityAPI.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)] + "updated"
             })
             .ToArray();
 
